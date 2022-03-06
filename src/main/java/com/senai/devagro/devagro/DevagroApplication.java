7b1d1b97d4e233ae6d525ae9338a3122029e9906
Leônidas Moritz Rosa <leonidas.rosa@senior.com.br>
2022-03-06 17:42:57 -0300

    Projeto Java Maven Spring Boot criado utilizando spring initializr com inicialmente as dependências do Spring Web, Spring Data, PostgreSQL Driver e Lombok.

---
+++ src/main/java/com/senai/devagro/devagro/DevagroApplication.java
@@ -0,0 +1,13 @@
+package com.senai.devagro.devagro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevagroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevagroApplication.class, args);
+	}
+
+}
